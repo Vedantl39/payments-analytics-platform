aa3e0517fdc0b673f05e11edb4c187f5a055608e
Vedant Limaye <109674630+Vedantl39@users.noreply.github.com>
2026-03-15 17:56:51 +0000

    Update analytics_queries.sql

--- sql/analytics_queries.sql
+++ sql/analytics_queries.sql
@@ -1,1 +1,11 @@
-
+CREATE VIEWS monthly_revenue_summary AS 
+SELECT 
+  DATE_TRUNC('month' as payment_date) as revenue_month,
+  SUM(total_price) AS total_revenue,
+  COUNT(DISTINCT payment_id) AS total_orders,
+  COUNT(DISTINCT customer_id) AS unique_customers,
+  AVG(total_price) AS average_order_value
+FROM fact_payments
+GROUP BY 1
+ORDER BY 1;
+  
